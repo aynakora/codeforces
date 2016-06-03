d5c2232860bb95bc799a70d3fb1bacc0aab2949f
Yujun Qin <yujun_qin@brown.edu>
2016-06-03 14:38:14 +0800

    Update 474A - Keyboard.cpp

--- 474A - Keyboard.cpp
+++ 474A - Keyboard.cpp
@@ -3,16 +3,10 @@
 using namespace std;
 int main() {
   char d;
-  string key[3] = {"qwertyuiop", "asdfghjkl;", "zxcvbnm,./"}, s, p;
+  string key = "qwertyuiopasdfghjkl;zxcvbnm,./", s, p;
   cin >> d >> s;
   int a = (d == 'L' ? 1 : -1);
   for (char& c : s) {
-    for (string& k : key) {
-      int f = k.find(c);
-      if (f != -1) {
-        cout << k.at(f + a);
-        break;
-      }
-    }
+    cout << key.at(key.find(c) + a);
   }
 }
